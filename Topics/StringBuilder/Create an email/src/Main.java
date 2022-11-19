18e85fa9f314c9b3ad45e99c51d2334330ae2942
denys <denamyte@gmail.com>
2022-11-19 12:35:42 +0200

    2022-11-19: 2 problems solved

---
+++ Topics/StringBuilder/Create an email/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class EmployeeManagement {
+
+    public static String createEmail(String name, String surname) {
+        return new StringBuilder(name).append(surname).append("@work.net").toString();
+    }
+
+    // Don't change the code below
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        String surname = scanner.next();
+
+        String completeEmail = createEmail(name, surname);
+
+        System.out.println(completeEmail);
+    }
+}
---
+++ Topics/StringBuilder/The English alphabet/src/Main.java
@@ -0,0 +1,6 @@
+class EnglishAlphabet {
+
+    public static StringBuilder createEnglishAlphabet() {
+        // write your code here
+    }
+}
